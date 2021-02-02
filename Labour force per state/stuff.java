b6ce30d624127864ddc5cc3e4ebca15799c2d036
Olabode-bod <75066623+Olabode-bod@users.noreply.github.com>
2021-02-02 19:13:48 +0100

    Add files via upload

---
+++ Labour force per state/stuff.java
@@ -0,0 +1,17 @@
+importjava.util.linkedList;
+
+public class GetElementExample{
+    public static void main(String[] args){
+        
+        linkedList<string> linkedlist = new LinkedList<string>();
+        linkedList.add("olabode");
+        linkedList.add("emmanuel");
+        linkedList.add("faith");
+         linkedList.add("joy");
+         linkedList.add("precious");
+        system.out.println("LinkedList elements : ");
+            
+        }
+    }
+
+}
